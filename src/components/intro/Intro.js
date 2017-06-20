644f940a24972a13a5705e7d594fcac8d9e2a6d7
Nathan Marasigan <egarsenal16@gmail.com>
2017-06-20 13:50:04 -0500

    added typist, utilized

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,16 +1,45 @@
 import React from "react";
 import Button from 'react-md/lib/Buttons';
+import Typist from 'react-typist';
 import './intro.css';
 
 function Intro(props) {
   return (
     <div className="block">
       <div className="row">
-        <p className="header">Hi, I'm Nathan</p>
+        <Typist 
+          className="header" 
+          avgTypingDelay={100}
+          cursor={{
+            show: true,
+            blink: true,
+            element: '_',
+            hideWhenDone: true, 
+            hideWhenDoneDelay: 50,
+            }}
+        >
+          Hi,
+        </Typist>
+        <Typist 
+          className="header"
+          startDelay={1000}
+          cursor={{
+            show: true,
+            blink:true,
+            element: '_',
+            hideWhenDone: true,
+            hideWhenDoneDelay: 1000
+          }}
+        >
+        &nbsp;I'm Nathan
+        </Typist>
       </div>
       <div className="row">
         <div className="flanker" />
-        <p className="subhead"> A student at the University of Illinois at Urbana Champaign interested in web development and communication networks.</p>
+        <p className="subhead">
+          A developer currently studying at 
+          <span style={{color:'#FA6300'}}> UIUC</span>
+        </p>
         <div className="flanker" />
       </div>
       <div className="buttons">
